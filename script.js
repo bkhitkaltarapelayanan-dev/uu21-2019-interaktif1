ac06c68d56ff14b7be18f1bee8cd9c9bcd12df71
bkhitkaltarapelayanan-dev <bkhitkaltarapelayanan@gmail.com>
2025-09-03 22:37:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ Dalam Undang-Undang ini yang dimaksud dengan:
     b. Pengeluaran Media Pembawa dari wilayah Negara Kesatuan Republik Indonesia,
     c. Pemasukan Media Pembawa dari suatu Area ke Area lain di dalam wilayah Negara Kesatuan Republik Indonesia dan
     d. Transit Media Pembawa.`,
-  4 : `Pasal 4
+  4: `Pasal 4
       Penyelenggaraan Karantina sebagaimana dimaksud dalam Pasal 3 dilakukan terhadap Media Pembawa yang diduga dapat membawa Hama dan Penyakit Hewan Karantina, Hama dan Penyakit Ikan Karantina, dan/atau Organisme Pengganggu Tumbuhan Karantina.`,
   5: `Pasal 5
       Setiap Media Pembawa yang dimasukkan ke dalam wilayah Negara Kesatuan Republik Indonesia wajib dilengkapi dengan sertifikat kesehatan dari negara asal dan dilakukan tindakan Karantina di Tempat Pemasukan.`,
@@ -131,6 +131,7 @@ function tampilkanPenjelasan(nomor) {
   div.style.display = "block";
   div.innerHTML = `<h2>Pasal ${nomor}</h2><p>${penjelasanPasal[nomor] || "Penjelasan belum tersedia."}</p>`;
 }
+
 
 
 
